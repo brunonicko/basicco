59081b32401f527f57fd6ed7de59e74ab236ce50
Bruno Nicko <brunonicko@gmail.com>
2021-10-19 21:59:13 -0700

    Frozen docstrings

--- basicco/frozen.py
+++ basicco/frozen.py
@@ -20,22 +20,14 @@ FROZEN_SLOT = "__isfrozeninstance__"
 
 
 @overload
-def frozen(
-    cls,
-    classes=None,
-    instances=None,
-):
+def frozen(cls, classes=None, instances=None):
     # type: (FMT, Optional[bool], Optional[bool]) -> FMT
     pass
 
 
 @overload
-def frozen(
-    cls=None,  # type: Literal[None]
-    classes=None,  # type: Optional[bool]
-    instances=None,  # type: Optional[bool]
-):
-    # type: (...) -> Callable[[FMT], FMT]
+def frozen(cls=None, classes=None, instances=None):
+    # type: (Literal[None], Optional[bool], Optional[bool]) -> Callable[[FMT], FMT]
     pass
 
 
@@ -53,7 +45,6 @@ def frozen(cls=None, classes=None, instances=None):
 
     :return: Decorated class.
     """
-
     classes = True if classes is None else bool(classes)
     instances = True if instances is None else bool(instances)
 
@@ -141,22 +132,33 @@ class FrozenMeta(type):
     @staticmethod
     def __new__(mcs, name, bases, dct, **kwargs):
         cls = super(FrozenMeta, mcs).__new__(mcs, name, bases, dct, **kwargs)
+
+        # Un-freeze this class momentarily.
         type.__setattr__(cls, _FROZEN_CLASS_INSTANCE_TAG, False)
+
         return cls
 
     def __init__(cls, name, bases, dct, **kwargs):
         super(FrozenMeta, cls).__init__(name, bases, dct, **kwargs)
+
+        # Freeze this class.
         type.__setattr__(
             cls, _FROZEN_CLASS_INSTANCE_TAG, getattr(cls, _FROZEN_CLASS_TAG, False)
         )
 
     def __call__(cls, *args, **kwargs):
         self = super(FrozenMeta, cls).__call__(*args, **kwargs)
+
         if getattr(cls, _FROZEN_OBJECT_TAG, False):
             if hasattr(self, "__dict__") or hasattr(cls, FROZEN_SLOT):
+
+                # Freeze this instance.
                 if not getattr(self, FROZEN_SLOT, False):
                     object.__setattr__(self, FROZEN_SLOT, True)
+
             else:
+
+                # Could not freeze slotted object, missing slot.
                 error = (
                     "could not freeze slotted instance of '{}.{}' since it doesn't "
                     "have a slot named {} (available as a constant at {}.{})"
@@ -168,6 +170,7 @@ class FrozenMeta(type):
                     "FROZEN_SLOT",
                 )
                 raise AttributeError(error)
+
         return self
 
     def __setattr__(cls, name, value):
