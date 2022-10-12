cb833726775d3cef4ebb3e0a7ab347bb34e537c5
Bruno Nicko <brunonicko@gmail.com>
2022-10-12 16:27:47 -0700

    Add set_to_none decorator for __hash__ methods

--- tests/test_explicit_hash.py
+++ tests/test_explicit_hash.py
@@ -1,7 +1,8 @@
 import pytest  # noqa
 import six
+import tippo
 
-from basicco.explicit_hash import ExplicitHash, ExplicitHashMeta
+from basicco.explicit_hash import ExplicitHash, ExplicitHashMeta, set_to_none
 
 
 def test_class():
@@ -28,6 +29,18 @@ def test_force_explicit_hash_declaration():
             raise NotImplementedError()
 
     assert SubClass
+
+
+def test_set_to_none():
+    class Class(object):
+        @set_to_none
+        def __hash__(self):
+            error = "not hashable"
+            raise TypeError(error)
+
+    assert not issubclass(Class, tippo.Hashable)
+    assert not isinstance(Class(), tippo.Hashable)
+    assert Class.__hash__ is None
 
 
 if __name__ == "__main__":
