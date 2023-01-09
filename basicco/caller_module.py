82b31ee749ac82fb39082ec8fe6f755bc4d2855e
Bruno Nicko <brunonicko@gmail.com>
2023-01-09 13:24:30 -0800

    Add auto_caller_module decorator (#49)

--- basicco/caller_module.py
+++ basicco/caller_module.py
@@ -1,8 +1,15 @@
 """Retrieve the caller's module name."""
 
+import functools
 import inspect
 
-__all__ = ["caller_module"]
+import six
+from tippo import Callable, Iterable, TypeVar
+
+__all__ = ["caller_module", "auto_caller_module"]
+
+
+T = TypeVar("T")
 
 
 def caller_module(frames=0):
@@ -26,3 +33,58 @@ def caller_module(frames=0):
                 return None
         else:
             return module.__name__
+
+
+def auto_caller_module(
+    iterable_params=None,  # type: Iterable[str] | str | None
+    single_params=None,  # type: Iterable[str] | str | None
+):
+    # type: (...) -> Callable[[T], T]
+    """
+    Make a decorator for a function that takes iterable/single keyword arguments that contain module paths.
+    If no paths are provided, set the caller module as a path.
+
+    :param iterable_params: Keyword arguments that are supposed to contain module paths.
+    :param single_params: Keyword argument that are supposed to contain a single module path or None.
+    :return: Decorator.
+    """
+    if isinstance(iterable_params, six.string_types):
+        iterable_params = (iterable_params,)
+    elif iterable_params is None:
+        iterable_params = ()
+    else:
+        iterable_params = tuple(iterable_params)
+
+    if isinstance(single_params, six.string_types):
+        single_params = (single_params,)
+    elif single_params is None:
+        single_params = ()
+    else:
+        single_params = tuple(single_params)
+
+    def decorator(func):
+        @functools.wraps(func)
+        def decorated(*args, **kwargs):
+            _iterable_params = []
+            _single_params = []
+            for iterable_param in iterable_params:
+                values = tuple(kwargs.get(iterable_param, ()))
+                if not values:
+                    _iterable_params.append(iterable_param)
+            for single_param in single_params:
+                value = kwargs.get(single_param, None)
+                if value is None:
+                    _single_params.append(single_param)
+
+            if _iterable_params or _single_params:
+                module = caller_module()
+                for iterable_param in _iterable_params:
+                    kwargs[iterable_param] = (module,)
+                for single_param in _single_params:
+                    kwargs[single_param] = module
+
+            return func(*args, **kwargs)
+
+        return decorated
+
+    return decorator
